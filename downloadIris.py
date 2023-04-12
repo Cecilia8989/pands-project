f79de7d6c6d1256487834ca7ab2e88af6a1d3cb6
Cecilia Pastore <ceciliapastore89@gmail.com>
2023-04-12 17:03:10 +0100

    Iris_data_set_csv

---
+++ downloadIris.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+
+# Download the iris data set 
+csv_ulr =  'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+
+# using the attribute inforamtion as the column names 
+col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
+iris =  pd.read_csv(csv_ulr, names = col_names)
+
+# save the dataset as CSV FILE 
+iris.to_csv("iris.csv", index=False)
+
