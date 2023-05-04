81b7246a28f01ba8ad0e7bf99aad268065bbcfe5
Cecilia Pastore <ceciliapastore89@gmail.com>
2023-05-04 22:29:42 +0100

    Project-change name

--- downloadIris.py
+++ downloadIris.py
@@ -1,22 +1,25 @@
 ''' This script will import the iris dataset from the url 
-'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data' 
+https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data' 
 and it convert it to a csv using pandas. Csv is save in the current directory
 '''
 
 # Author: Cecilia Pastore 
 # downloadiris.py 
 
+# Import pandas library
 import pandas as pd 
 
-# Download the iris data set 
+# Define the URL from which to download the dataset 
 csv_ulr =  'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
-# using the attribute inforamtion as the column names 
+# Set the column names of the dataset to correspond with the attribute information 
 col_names = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width','Class']
+# download the dataset and change the colums 
 iris =  pd.read_csv(csv_ulr, names = col_names)
 
-# save the dataset as CSV FILE 
+# Save the dataset as a CSV file 
 iris.to_csv("iris_dataset.csv", index=False)
+print("iris_dataset.csv created")
 
 
 
